b1027ffda5236bc11639b799c3a6ac4bec62edb0
Coris12 <corina.guzman.est@tecazuay.edu.ec>
2021-12-05 21:37:43 +0700

    creacion de las clases

--- src/main/java/com/Caso1Backend/back/security/models/Rol.java
+++ src/main/java/com/Caso1Backend/back/security/models/Rol.java
@@ -15,10 +15,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-/**
- *
- * @author LENOVO
- */
+
 @Entity
 public class Rol {
 
