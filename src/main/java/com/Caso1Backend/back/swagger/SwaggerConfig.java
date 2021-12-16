ba6011c311043e29d0bf96cfc23f5b82103123c2
Coris12 <corina.guzman.est@tecazuay.edu.ec>
2021-12-16 13:55:32 +0700

    creacion de la clase producto
    producto

--- src/main/java/com/Caso1Backend/back/swagger/SwaggerConfig.java
+++ src/main/java/com/Caso1Backend/back/swagger/SwaggerConfig.java
@@ -8,6 +8,7 @@ package com.Caso1Backend.back.swagger;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -21,6 +22,7 @@ import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
 
 /**
  *
